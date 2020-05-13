31d4e8272e026d311548f370674fea0a0fe10b66
koniman <user@USER-no-MacBook-Pro.local>
2020-05-13 12:20:08 +0900

    chat

---
+++ app/assets/javascripts/channels/room.js
@@ -0,0 +1,27 @@
+App.room = App.cable.subscriptions.create("RoomChannel", {
+  connected: function() {
+    console.log('connected')
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(message) {
+    alert(message)
+  },
+  speak: function(content) {
+    // messageに引数のcontentで受け取った値を渡す
+    return this.perform('speak', {message: content});
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const input = document.getElementById('chat-input')
+  const button = document.getElementById('button')
+  button.addEventListener('click', function() {
+    const content = input.value
+    App.room.speak(content)
+    input.value = ''
+  })
+})
